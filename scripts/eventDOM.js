820abccbca4e1bb1d31bff87a211bf2329ac7d6c
Ashish Garg <mragarg@gmail.com>
2019-03-15 11:34:06 -0400

    FIXED

--- scripts/eventDOM.js
+++ scripts/eventDOM.js
@@ -29,18 +29,18 @@ function getUserInput() {
     });
 }
 
-
 getUserInput();
 
-
+////////////////////////////////////////////////////////////////
 //////////////////////// SUBMIT BUTTON ////////////////////////
+////////////////////////////////////////////////////////////////
+
 // Submit Button DOM
 const submitButton = document.querySelector("[data-submitButton]");
 
-
 // After User selects meal and diet, when clicking submit, it takes in the values and calls Recipe API
 function submitButtonRetrieve() {
-    // retrieveData(getRelevantData(mealFinal, vegetarianFinal));
+    retrieveData(getRelevantData(mealFinal, vegetarianFinal));
     console.log(`Test. ${mealFinal} Test. ${vegetarianFinal}`)
     // setRecipeImage();
 }
@@ -48,38 +48,39 @@ function submitButtonRetrieve() {
 // submitButton Event Listener
 submitButton.addEventListener("click", submitButtonRetrieve);
 
+
+////////////////////////////////////////////////////////////////
 //////////////////////// RECIPE IMAGE ////////////////////////
+////////////////////////////////////////////////////////////////
+
 // recipeImage = recipe image DOM
 const recipeImage = document.querySelector("[data-recipeIMG]");
 
 // setRecipeImage function sets the src of the recipeImage DOM to the image of the recipe
 function setRecipeImage(){
-
     recipeImage.src = cleanRecipeInfo[5]; // NOTE CHANGE 0 TO ELEMENT WITH IMAGE SOURCE
 }
 
 
-///////////////////////////////////////////////////////////////////////////////////
-//////////////////////////Nutrition button function //////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////Nutrition button function /////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 /// Nutrition button should retrieve nutritional info from array and pass it on to
 /// fetch nutrition. Nutrition button on click Should run this function. 
 function passNutInfo() {
-    fetchNutrition(cleanRecipeInfo[1])
-    const NutInfo = document.querySelector('[nut-info]');
-
-    NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`
-
-    NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`
-
+    fetchNutrition(cleanRecipeInfo[1]);
+    // NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`;
 }
 
 ///Function sets text content of nut info to blank and adds event listener to Nut Button. Upon clicking
 ///Nut button, fetch nutrition function will run and make api call and will add variables
-function drawNutInfo() {
-    const NutButton = document.querySelector('[nut-button')
 
-    NutButton.addEventListener('click', passNutInfo())
+const NutInfo = document.querySelector('[nut-info]');
+const NutButton = document.querySelector('[nut-button');
+NutButton.addEventListener('click', passNutInfo);
 
+
+function drawNutritionLabel(){
+    NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`;
 }
