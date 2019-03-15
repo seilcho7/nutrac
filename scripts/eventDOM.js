c37ca0792497bfe6111b5285068af2090c263d49
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-15 10:54:19 -0400

    adds image URL, Nutrition button dom element, and function that adds event listener to Nutrition button to show updated nutrition info

--- scripts/eventDOM.js
+++ scripts/eventDOM.js
@@ -29,4 +29,32 @@ function getUserInput() {
     });
 }
 
+
 getUserInput();
+
+
+///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////Nutrition button function //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/// Nutrition button should retrieve nutritional info from array and pass it on to
+/// fetch nutrition. Nutrition button on click Should run this function. 
+
+
+function passNutInfo() {
+    fetchNutrition(cleanRecipeInfo[1])
+    const NutInfo = document.querySelector('[nut-info]');
+
+    NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`
+
+    NutInfo.textContent = `Calories: ${totalCalories} Trans Fats: ${totalTFat} Saturated Fat: ${totalSFat} Cholesterol: ${totalCholesterol} Sodium: ${totalSodium} Carbohydrates: ${totalCarbohydrates} Fiber: ${totalDFiber} Sugar: ${totalSugar} Protein: ${totalProtein} Potassium: ${totalPotassium}`
+
+}
+///Function sets text content of nut info to blank and adds event listener to Nut Button. Upon clicking
+///Nut button, fetch nutrition function will run and make api call and will add variables
+function drawNutInfo() {
+    const NutButton = document.querySelector('[nut-button')
+
+    NutButton.addEventListener('click', passNutInfo())
+
+}
