5ea0ad2dab75b98a414372c28b4961c0c7cfba49
Ashish Garg <mragarg@gmail.com>
2019-03-19 14:50:19 -0400

    Fix restaurant duplicate:

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,1 @@
-// getUserInput();
-// getRelevantData(mealFinal, vegetarianFinal);
-
-// Main function in order of execution
-// function main() {
-//     getRelevantData(); 
-//     retrieveData();
-// }
-
-// console.log("User input is lunch and vegetarian. Printing URL");
-// console.log(getRelevantData(mealFinal, vegetarianFinal));
-
-// console.log("Putting URL into fetch and assigning it to a variable (recipeInfoArray)")
-// retrieveData(getRelevantData("lunch", "%2Cvegetarian"));
-
-// console.log("Create array to use (testRecipeCLEAN)");
-
+getLocation();
